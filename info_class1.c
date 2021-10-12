44d842a225ca058fa9216a20e96666f5f6703bd8
j4pngthr <weibeicun@gmail.com>
2021-10-12 15:17:37 +0900

    乗除をできるようにしました

--- info_class1.c
+++ info_class1.c
@@ -1,54 +1,76 @@
+// add *, /
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 int lookahead;
 
 void error() {
-	printf("syntax error\n");
-	exit(1);
+  printf("syntax error\n");
+  exit(1);
 }
 
 
-void match(int t) {
-	if (lookahead == t) {
-		lookahead = getchar();
-		// printf("\"%c\"\n", lookahead); // last "\n"
-	} else {
-		error();
-	}
+void match(int t) { // get next char
+  if (lookahead == t) {
+    lookahead = getchar();
+    // printf("\"%c\"\n", lookahead); // last "\n"
+  } else {
+    error();
+  }
+}
+
+void factor() { // leaf, output number and get operator
+  if (isdigit(lookahead)) {
+    putchar(lookahead);
+    match(lookahead);
+  } else {
+    error();
+  }
 }
 
 void term() {
-	if (isdigit(lookahead)) {
-		putchar(lookahead);
-		match(lookahead);
-	} else {
-		error();
-	}
+  factor();
+  while (1) {
+    if (lookahead == '*') {
+      match('*');
+      factor();
+      putchar('*');
+    } else if (lookahead == '/') {
+      match('/');
+      factor();
+      putchar('/');
+    } else if (lookahead == '+' || lookahead == '-' || lookahead == '\n') {
+      break;
+    } else {
+      error();
+    }
+  }
 }
 
 void expr() {
-	term();
-	while (1) {
-		if (lookahead == '+') {
-			match('+');
-			term();
-			putchar('+');
-		} else if (lookahead == '-') {
-			match('-');
-			term();
-			putchar('-');
-		} else { // "\n", (work with garbage input)
-			break;
-		}
-	}
+  term();
+  while (1) {
+    if (lookahead == '+') {
+      match('+');
+      term();
+      putchar('+');
+    } else if (lookahead == '-') {
+      match('-');
+      term();
+      putchar('-');
+    } else if (lookahead == '\n') {
+      break;
+    } else { // changed
+      error();
+    }
+  }
 }
 
 
 int main(void) {
-	lookahead = getchar();
-	expr();
-	putchar('\n');
-	return 0;
+  lookahead = getchar();
+  expr();
+  putchar('\n');
+  return 0;
 }
-
