89ea9e09491c02d7105e9a5bc6af57ff9d26598b
j4pngthr <weibeicun@gmail.com>
2021-10-13 13:09:48 +0900

    \nの追加

--- info_class2/emitter.c
+++ info_class2/emitter.c
@@ -4,19 +4,19 @@
 void emit(int t, int tval) {
   switch(t) {
     case '+': case '-': case '*': case '/':
-      printf("%c", t);
+      printf("%c\n", t);
       break;
     case DIV:
-      printf("DIV");
+      printf("DIV\n");
       break;
     case MOD:
-      printf("MOD");
+      printf("MOD\n");
       break;
     case NUM:
-      printf("%d", tval);
+      printf("%d\n", tval);
       break;
     case ID:
-      printf("%s", symtable[tval].lexptr);
+      printf("%s\n", symtable[tval].lexptr);
       break;
     default:
       printf("token %d, tokenval %d\n", t, tval);
