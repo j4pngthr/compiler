9a9e87cadc82b9ab04d86f176d6d87e874bdfb57
j4pngthr <weibeicun@gmail.com>
2021-10-14 16:55:42 +0900

    コメントアウトの削除

--- info_class2/main.c
+++ info_class2/main.c
@@ -8,6 +8,7 @@ char scanword[256];
 
 void main() {
   init();
+  
   printf("ファイル名:");
   char filename[BUFFER_SIZE];
   scanf("%s", filename);
@@ -15,23 +16,6 @@ void main() {
     printf("ファイルオープン失敗\n");
     return;
   }
-  // char c;
-  // while ((c = fgetc(fp)) != EOF) {
-  //   fputc(c, stdin);
-  //   c = getchar();
-  //   printf("%c\n", c);
-  // }
-  // while (fgets(scanword, BUFFER_SIZE, fp) != NULL) {
-  //   fputs(scanword, stdin);
-  //   char s[256];
-  //   scanf("%s", &s);
-  //   printf("%s\n", scanword);
-  // }
-  // char c = fgetc(fp);
-  // putchar(c);
-  // c = fgetc(fp);
-  // putchar(c);
-
 
   parse();
 }
